4def7a8d6dae9223734fa29cd576e04599cad272
Beshoy S. Said <72396606+BeshoyS@users.noreply.github.com>
2023-01-12 18:50:45 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Avion Ecommerce</h1>
+      <p>Under Constructions</p>
+    </div>
+  );
+}
+
+export default App;
